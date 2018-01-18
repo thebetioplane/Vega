f6e0fd0375b7008262eddc2a0a50c9cdc26d4e3b
thebetioplane <thebetioplane@users.noreply.github.com>
2018-01-17 23:06:52 -0500

    2018.01.17 23.06

--- make_release.sh
+++ make_release.sh
@@ -10,7 +10,7 @@ bundler_exe=../AssetBundler/bin/Release/AssetBundler.exe
 chmod +x $bundler_exe
 $bundler_exe ../AssetBundler/assets/img VegaImg.dat
 $bundler_exe ../AssetBundler/assets/snd VegaSnd.dat
+wine ../packages/peupdate.bin -rc Vega.exe
 chmod +x Vega.exe
 ./Vega.exe make-index
 rm -f *.log
-wine ../packages/peupdate.bin -rc Vega.exe
