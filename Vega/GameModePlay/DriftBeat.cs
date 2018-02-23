bd884fcda3f3b06ea175f69ace4d4f8f4723e700
thebetioplane <thebetioplane@users.noreply.github.com>
2018-02-22 21:28:19 -0500

    2018.02.22 21.28

--- Vega/GameModePlay/DriftBeat.cs
+++ Vega/GameModePlay/DriftBeat.cs
@@ -1,3 +1,7 @@
+using System;
+using OpenTK;
+using OpenTK.Graphics;
+
 namespace Vega.GameModePlay
 {
     public class DriftBeat : Beat
@@ -20,9 +24,19 @@ namespace Vega.GameModePlay
             Assert(this.Row < 4, "DriftBeat: Start row too large");
             Assert(this.RowEnd < 4, "DriftBeat: End row too large");
         }
+        private static readonly Vector2 BeatOrigin = new Vector2(64.0f, 64.0f);
         public override void Draw(double t0, double t1)
         {
-
+            float y0 = (float)(20.0 + 800.0 * t0);
+            float y1 = (float)(20.0 + 800.0 * t1);
+            float x0 = this.Row * 200.0f + 200.0f;
+            float x1 = this.RowEnd * 200.0f + 200.0f;
+            Console.WriteLine("x: {0}, y: {1}", (x0 - x1) / 200.0f, (y0 - y1) / 200.0f);
+            for (int i = 0; i < 200; ++i)
+            {
+                float f = i / 200.0f;
+                Assets.Body.Draw(x1 + (x0 - x1) * f, y1 + (y0 - y1) * f, BeatOrigin, Color4.White);
+            }
         }
 
         public override void Draw(double t0)
--- Vega/GameModePlay/ShortBeat.cs
+++ Vega/GameModePlay/ShortBeat.cs
@@ -9,15 +9,16 @@ namespace Vega.GameModePlay
             : base (BeatType.Short, start, end, row)
         {
         }
-        private static Vector2 BeatOrigin = new Vector2(23.0f, 0.0f);
+        private static Vector2 BeatOrigin = new Vector2(25.0f, 25.0f);
+        private static Vector2 MidOrigin = new Vector2(25.0f, 0.0f);
         public override void Draw(double t0, double t1)
         {
             float x = this.Row * 100.0f + 600.0f;
             float y0 = (float)(20.0 + 800.0 * t0);
             float y1 = (float)(20.0 + 800.0 * t1);
-            Assets.BlueBeatMid.Draw(x, y1, 50.0f, y1 - y0, BeatOrigin, Color4.White);
-            Assets.BlueBeat.Draw(x, y0, 50.0f, y0, BeatOrigin, Color4.White);
-            Assets.BlueBeat.Draw(x, y1, 50.0f, y0, BeatOrigin, Color4.White);
+            Assets.BlueBeat.Draw(x, y0, 50.0f, 50.0f, BeatOrigin, Color4.White);
+            Assets.BlueBeat.Draw(x, y1, 50.0f, 50.0f, BeatOrigin, Color4.White);
+            Assets.BlueBeatMid.Draw(x, y0, 50.0f, y1 - y0, MidOrigin, Color4.White);
         }
         public override void Draw(double t0)
         {
